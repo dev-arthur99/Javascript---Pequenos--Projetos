49412ccbf8f039e72fcb5e3f248583096795a16e
Arthur Dias <dev.arthur99@gmail.com>
2020-09-22 11:59:31 -0300

    First commit

---
+++ Auto-Write-Text/script.js
@@ -0,0 +1,15 @@
+const text = 'Meus seguidores do Twitch são os melhores!!';
+
+let index = 0;
+
+function writeText() {
+  document.body.innerText = text.slice(0, index);
+
+  index++;
+
+  if (index > text.length) {
+    index = 0;
+  }
+}
+
+setInterval(writeText, 100);
